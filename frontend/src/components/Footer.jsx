aa9c8bd94056782dd7d8366e5a58117fb084c01f
Amit Giri <amitgiri1281@gmail.com>
2025-11-02 09:08:53 +0530

    Upadte footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Niti IAS Counselling Institute. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Niti IAS Coaching Institute. All rights reserved.</p>
         </div>
       </div>
     </footer>
